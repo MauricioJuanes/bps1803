8e87e15ba44fc15e4b43ede7ab08c64fdafa04e1
Yasbedh Quiñones <yabodev@gmail.com>
2018-09-28 22:12:43 -0500

    Añadiendo list view con opciones para invocar otros activity y mostrar datos al finalizar dichos acivity.

--- app/src/main/java/com/prototipo/prototipo/prototipo/MainActivity.java
+++ app/src/main/java/com/prototipo/prototipo/prototipo/MainActivity.java
@@ -2,12 +2,35 @@ package com.prototipo.prototipo.prototipo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    //List of options for the main view
+    private List<CustomItemList> optionList;
+
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //initializing objects
+        optionList = new ArrayList<>();
+        listView = (ListView) findViewById(R.id.options_list_view);
+
+        //adding some values to our list
+        optionList.add(new CustomItemList("Área de un local", "000 m2"));
+        optionList.add(new CustomItemList("Histórico CFE", "Captura los datos de tu recibo"));
+
+        //creating the adapter
+        CustomListAdapter adapter = new CustomListAdapter(this, R.layout.item_list_layout, optionList);
+
+        //attaching adapter to the listview
+        listView.setAdapter(adapter);
     }
 }
