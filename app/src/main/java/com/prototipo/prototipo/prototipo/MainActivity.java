a103364af9bfadd63925aa1fa7b0f2d54eca5dda
Yasbedh Quiñones <yabodev@gmail.com>
2018-10-01 19:20:08 -0500

    Cambio del nombre de la aplicación y configuracion de iconos del launcher y titulo de la vista principal

--- app/src/main/java/com/prototipo/prototipo/prototipo/MainActivity.java
+++ app/src/main/java/com/prototipo/prototipo/prototipo/MainActivity.java
@@ -12,6 +12,7 @@ import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -100,6 +101,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Adding icon aside app name
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setIcon(R.drawable.logo);
+
         database = new Database(this.getApplicationContext());
         gson = new Gson();
         boton_historico_cfe = findViewById(R.id.btn_historico_cfe);
